48b127e4954ece6d78de365a059d3b047d576f32
Christopher Huei <chris.huei561@gmail.com>
2022-04-19 22:24:15 -0400

    Final Project Command Line

---
+++ game.py
@@ -0,0 +1,79 @@
+def intro():
+    print("Welcome to the world of Pokemon! You are a trainer living your dream of becoming a Pokemon Master! Are you ready? Your adventure is about to begin!")
+    print("Your goal is collect and capture Pokemon until you stop the game or get bored.")
+    ready = input("Are you ready?: y/n ")
+    if ready =='y':
+        print("Lets begin your adventure!")
+    else:
+        print('Goodbye')
+intro()
+
+def encounter1():
+    print("You're walking through the forest and a WILD PIKACHU APPEARS.")
+    print("You flip your hat around and stare at the Pokemon with intense fiery eyes, readying your Pokeball.")
+encounter1()
+
+def throwBall1():
+    ready = input("Throw ball?: y/n ")
+    if ready =='y':
+        print("You caught it!")
+    else:
+        print("You safely ran away.")
+throwBall1()
+
+def encounter2():
+    print("After catching your first Pokemon, you encounter a Charmander.")
+    print("You believe that having a fire type Pokemon will definitely come in handy in the future.")
+encounter2()
+
+def throwBall2():
+    ready = input("Throw ball?: y/n ")
+    if ready =='y':
+        print("You caught it!")
+    else:
+        print("Your ball broke!")
+throwBall2()
+
+def encounter3():
+    print("You let Charmander out of its ball and what? Charmander is evolving??")
+    input("Congratulations! Your Charmander has evolved into Charmeleon! But it doesnt look happy....")
+    input("In one big breath, Charmeleon used Flamethrower...on your face....just like in the show....now your face is charred....")
+    input("While writhing in pain, you walk into some grass and oh look! You found Burn Heal! Thatll definitely come in handy!")
+encounter3()
+
+def encounter4():
+    print("While walking through the forest you find a Potion.")
+    input("You put the Potion in your bag, this may be useful in the future.")
+    input("While trying to find anymore useful items, you see a very tall shadowy figure float towards you....")
+    input("OH NO! ITS MEWTWO!")
+encounter4()
+
+def throwBall3():
+    ready = input("Throw ball?: y/n ")
+    if ready =='y':
+        print("The ball broke and you ran for your life!")
+    else:
+        print("You missed and you ran for your life!")
+throwBall3()
+
+
+
+
+
+
+
+    
+#how to code out if the pokemon escapes ball or is caught
+####### input stops execution of program, goes onto next part of code
+#define random item found
+
+#### F5 to run terminal
+
+#parameters
+
+#to play the game type in python3 then py file
+
+
+
+
+    
